39c96a86f449338c9b732993ff01a47190787838
HarshithaC30 <47312530+HarshithaC30@users.noreply.github.com>
2019-03-06 18:22:44 +0530

    Add files via upload

---
+++ Spring_Aop/src/main/java/com/cg/aop/Spring_Aop/App.java
@@ -0,0 +1,13 @@
+package com.cg.aop.Spring_Aop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Spring_Aop/src/test/java/com/cg/aop/Spring_Aop/AopMain.java
@@ -0,0 +1,38 @@
+package com.cg.aop.Spring_Aop;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AopMain 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AopMain( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AopMain.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
